cb893b249f1f91f5de3656bfd6f7f198139fffab
arslan0207 <muhammad.arslan0207@gmail.com>
2025-06-27 09:21:32 +0400

    Add next.config.desktop.js 1751001691214

---
+++ next.config.desktop.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: 'out',
+  images: {
+    unoptimized: true
+  },
+  assetPrefix: './',
+  experimental: {
+    esmExternals: false
+  }
+}
+
+module.exports = nextConfig
